a9cdc266f7236f624a39fec642d230224eab4772
a-programmer-guy <ctrl_kay@protonmail.com>
2022-08-31 19:40:21 -0600

    Add First Test - Login Feature
    This test checks if a user can log in with a valid username and password

---
+++ src/test/java/testcase/LoginFeature.java
@@ -0,0 +1,27 @@
+package testcase;
+
+import javax.xml.xpath.XPath;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginFeature {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.saucedemo.com/");
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+		String expected = "PRODUCTS";
+		String actual = driver.findElement(By.xpath("//*[@id='header_container']/div[2]/span[@class='title']")).getText();
+		Assert.assertEquals(actual, expected);
+		driver.quit();
+		driver.close();
+	}
+}
